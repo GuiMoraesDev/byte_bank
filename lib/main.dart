2526a0b31a889196e07a8bcc8ed0d9189a199673
Guilherme Moraes <guimoraes.devv@gmail.com>
2022-04-30 17:18:53 -0300

    feat: add usage of List View at home page

--- lib/main.dart
+++ lib/main.dart
@@ -5,8 +5,22 @@ void main() {
     MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Transfers'),
+          title: const Text(
+            'Transfers',
+          ),
         ),
+        body: ListView(children: const <Widget>[
+          Card(
+            child: ListTile(
+              leading: Icon(Icons.monetization_on),
+              title: Text('US\$ 10,000'),
+              subtitle: Text(
+                'Ten thousand dollars',
+              ),
+              trailing: Icon(Icons.delete),
+            ),
+          ),
+        ]),
         floatingActionButton: const FloatingActionButton(
           child: Icon(Icons.add),
           onPressed: null,
